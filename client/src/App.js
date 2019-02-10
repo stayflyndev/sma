f2a30e0a892f07fbd3258987929454462b9473d2
yourtechsis <aretee64@gmail.com>
2019-02-10 06:06:12 -0700

    routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import Link from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
+
+import Register from './components/Auth/Register';
+import Login from './components/Auth/Login';
+
 
 
 
@@ -10,12 +16,18 @@ import './App.css';
 class App extends Component {
   render() {
     return (
+     
+      <Router>
       <div className="App">
-   
       <Navbar /> 
-      <Landing /> 
+      <Route exact path ='/' component={Landing} />
+      <div className="container">
+      <Route exact path= "/register" component={Register} />
+      <Route exact path= "/login" component={Login} />
+      </div>
      <Footer /> 
       </div>
+   </Router>
     );
   }
 }
