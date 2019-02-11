3ec769330dcc515f8bc9847e5a4541f5e8d86128
yourtechsis <aretee64@gmail.com>
2019-02-10 20:53:34 -0700

    updates

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,11 +1,60 @@
 import React, { Component } from 'react'
 
 class Login extends Component {
+    constructor(){
+        super();
+        this.state = {
+            email: '',
+            password: '',
+            errors: {}
+          
+        };
+        // this.onChange = this.onChange.bind(this)
+        this.onSubmit = this.onSubmit.bind(this)
+    
+    }
+    
+    onChange = (event) =>
+    {
+        this.setState({[event.target.name]: event.target.value}) //name is the attribute of the input form
+    }
+    
+    onSubmit(event){
+    event.preventDefault();
+    const user = {
+        email: this.state.email,
+        password: this.state.password,
+    }
+    console.log(user);
+    
+    }
+
+
   render() {
     return (
-      <div>
-        <h1>Login</h1>
-      </div>
+   
+        <div className="login">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-8 m-auto">
+                <h1 className="display-4 text-center">Log In</h1>
+                <p className="lead text-center">Sign in to your b.Social account</p>
+                <form onSubmit={this.onSubmit}>
+                  <div className="form-group">
+                    <input type="email" className="form-control form-control-lg" placeholder="Email Address" name="email" value={this.state.email} onChange={this.onChange} onSubmit={this.onSubmit} />
+                  </div>
+                  <div className="form-group">
+                    <input type="password" className="form-control form-control-lg" placeholder="Password" name="password" value={this.state.password} onChange={this.onChange} onSubmit={this.onSubmit} />
+                  </div>
+                  <input type="submit" className="btn btn-info btn-block mt-4" />
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      
+
+
     )
   }
 }
--- client/src/components/Auth/Register.js
+++ client/src/components/Auth/Register.js
@@ -1,11 +1,70 @@
 import React, { Component } from 'react'
 
 class Register extends Component {
+constructor(){
+    super();
+    this.state = {
+        name: '',
+        email: '',
+        password: '',
+        passwordtwo:'' ,
+        errors: {}
+      
+    };
+    this.onChange = this.onChange.bind(this)
+    this.onSubmit = this.onSubmit.bind(this)
+
+}
+
+onChange(event)
+{
+    this.setState({[event.target.name]: event.target.value}) //name is the attribute of the input form
+}
+
+onSubmit(event){
+event.preventDefault();
+const newUser = {
+    name: this.state.name,
+    email: this.state.email,
+    password: this.state.password,
+    passwordtwo: this.state.passwordtwo
+}
+console.log(newUser);
+
+}
+
+
+
   render() {
     return (
-      <div>
-        <h1>Register</h1>
+     
+        <div className="register">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Sign Up</h1>
+              <p className="lead text-center">Create your b.Social account</p>
+              <form onSubmit= {this.onSubmit}>
+                <div className="form-group">
+                  <input type="text" className="form-control form-control-lg" placeholder="Name" name="name" value={this.state.name} onChange={this.onChange} />
+                </div>
+                <div className="form-group">
+                  <input type="email" className="form-control form-control-lg" placeholder="Email Address" name="email" value={this.state.email} onChange={this.onChange} />
+                  <small className="form-text text-muted">This site uses Gravatar so if you want a profile image, use a Gravatar email</small>
+                </div>
+                <div className="form-group">
+                  <input type="password" className="form-control form-control-lg" placeholder="Password" name="password" value={this.state.password} onChange={this.onChange}/>
+                </div>
+                <div className="form-group">
+                  <input type="password" className="form-control form-control-lg" placeholder="Confirm Password" name="passwordtwo" value={this.state.passwordtwo} onChange={this.onChange}/>
+                </div>
+                <input type="submit" className="btn btn-info btn-block mt-4" />
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
+    
     )
   }
 }
